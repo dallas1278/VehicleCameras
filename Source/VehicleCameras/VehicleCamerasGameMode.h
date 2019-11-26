7c0cb04f80f47502a39b5973576fa899b06ad7f6
Dallas1278 <dallas.stowasser@gmail.com>
2019-11-26 06:35:47 -0600

    push 2

--- Source/VehicleCameras/VehicleCamerasGameMode.h
+++ Source/VehicleCameras/VehicleCamerasGameMode.h
@@ -1,6 +1,6 @@
 // Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
 #pragma once
-#include "GameFramework/GameModeBase.h"
+#include "GameFramework/GameModeBase.h"d
 #include "VehicleCamerasGameMode.generated.h"
 
 UCLASS(minimalapi)
